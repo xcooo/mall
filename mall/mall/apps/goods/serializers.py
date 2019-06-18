eddc1a56d224b562c8b7f5c434e930581f9812cc
新城 <602006050@qq.com>
2019-06-18 21:53:13 +0800

    视图索引实现

--- mall/mall/apps/goods/serializers.py
+++ mall/mall/apps/goods/serializers.py
@@ -8,6 +8,9 @@
 from rest_framework import serializers
 from .models import SKU
 
+from drf_haystack.serializers import HaystackSerializer
+from .search_indexes import SKUIndex
+
 class SKUSerializer(serializers.ModelSerializer):
     '''
     SKU商品序列化器
@@ -16,3 +19,11 @@ class SKUSerializer(serializers.ModelSerializer):
         model = SKU
         fields = ('id', 'name', 'price', 'default_image_url', 'comments')
 
+
+class SKUIndexSerializer(HaystackSerializer):
+    """
+    SKU索引结果数据序列化器
+    """
+    class Meta:
+        index_classes = [SKUIndex]
+        fields = ('text', 'id', 'name', 'price', 'default_image_url', 'comments')
