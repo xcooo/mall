0f32ca566267de27b42c4db31ee9d9fbe0c1b6fa
新城 <602006050@qq.com>
2019-06-20 09:26:44 +0800

    购物车全选后端实现

--- mall/mall/apps/carts/urls.py
+++ mall/mall/apps/carts/urls.py
@@ -10,6 +10,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^cart/$', views.CartView.as_view())
+    url(r'^cart/$', views.CartView.as_view()),
+    url(r'^cart/selection/$', views.CartSelectAllView.as_view())
 ]
 
